685ad22c7b50c8814d0bdfe93a7ca0c0c5dd20f6
roy. <roywashington1547@gmail.com>
2024-09-20 21:30:07 -0500

    trpc

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,18 @@
-import { Elysia } from 'elysia';
+import { Elysia, t as T } from 'elysia';
+import { compile as c, trpc } from '@elysiajs/trpc';
+import { initTRPC } from '@trpc/server';
+const trpcInstance = initTRPC.create();
+const procedure = trpcInstance.procedure;
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const router = trpcInstance.router({
+  greet: procedure
+    .input(c(T.String()))
+    .query(({ input }) => `Hello, ${input}!`),
+});
+
+export type Router = typeof router;
+
+const app = new Elysia().use(trpc(router)).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
