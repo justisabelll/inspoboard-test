dce758391fbe89c62759cb09a806630e20c8d1c9
roy. <roywashington1547@gmail.com>
2024-09-20 23:00:48 -0500

    switch to hono lol

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,9 @@
-import { Elysia, t as T } from 'elysia';
-import { compile as c, trpc } from '@elysiajs/trpc';
-import { initTRPC } from '@trpc/server';
-const trpcInstance = initTRPC.create();
-const procedure = trpcInstance.procedure;
+import { Hono } from 'hono';
 
-const router = trpcInstance.router({
-  greet: procedure
-    .input(c(T.String()))
-    .query(({ input }) => `Hello, ${input}!`),
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
 });
 
-export type Router = typeof router;
-
-const app = new Elysia().use(trpc(router)).listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export default app;
